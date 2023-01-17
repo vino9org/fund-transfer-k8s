9688df5b77fc3bccef15f25d086ce180d54e021b
Li Lin <guru.lin@gmail.com>
2023-01-17 22:45:11 +0800

    enabled retry

--- src/main/java/net/vino9/vinobank/client/CoreBankingService.java
+++ src/main/java/net/vino9/vinobank/client/CoreBankingService.java
@@ -5,11 +5,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import net.vino9.vinobank.FundTransferRequest;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.eclipse.microprofile.faulttolerance.Retry;
 
 @Path("/core-banking")
 @RegisterRestClient(configKey="core-banking-api")
 public interface CoreBankingService {
     @POST
     @Path("/local-transfers")
+    @Retry(maxRetries = 3, delay=200, jitter=100)
     Response invokeFundTransfer(FundTransferRequest payloadToSend);
 }
